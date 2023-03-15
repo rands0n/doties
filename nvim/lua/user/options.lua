431046863c713b1ff99c3a0e06538fda016af25f
Randson <orandson@gmail.com>
2023-03-15 13:06:31 +0000

    configure nvim with search defaults

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,30 @@
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.title = true
+vim.opt.termguicolors = true
+vim.opt.spell = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.breakindent = true -- maintain indent when wrapping indented lines
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = '▸ ', trail = '·' }
+vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
+vim.opt.mouse = 'a' -- enable mouse for all modes
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 5
+vim.opt.clipboard = 'unnamedplus' -- Use Linux system clipboard
+vim.opt.confirm = true -- ask for confirmation instead of error
+vim.opt.undofile = true -- persistent undo
+vim.opt.backup = true -- automatically save a backup file
+vim.opt.backupdir:remove('.') -- keep backups out of the current directory
+vim.opt.shortmess:append({ I = true }) -- disable the splash screen
+vim.opt.wildmode = 'longest:full,full' -- complete the longest common match, and allow tabbing the results to fully complete them
+
---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,124 @@
+local ensure_packer = function ()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+
+    return true
+  end
+
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').reset()
+require('packer').init({
+  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
+})
+
+local use = require('packer').use
+
+use('wbthomason/packer.nvim') -- Let packer manage itself
+
+-- Custom plugins
+
+use('tpope/vim-commentary')
+use('tpope/vim-surround')
+use('tpope/vim-repeat')
+use('tpope/vim-sleuth')
+use('tpope/vim-eunuch')
+use('sheerun/vim-polyglot')
+use('christoomey/vim-tmux-navigator')
+use('nelstrom/vim-visual-star-search')
+use('farmergreg/vim-lastplace')
+
+-- using packer.nvim
+use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+
+use({
+  'airblade/vim-rooter',
+  setup = function()
+    vim.g.rooter_manual_only = 1
+  end,
+  config = function()
+    vim.cmd('Rooter')
+  end,
+})
+
+use({
+  'windwp/nvim-autopairs',
+  config = function()
+    require('nvim-autopairs').setup()
+  end,
+})
+
+use({
+  'karb94/neoscroll.nvim',
+  config = function()
+    require('neoscroll').setup()
+  end,
+})
+
+use({
+  'nvim-telescope/telescope.nvim',
+  requires = {
+    { 'nvim-lua/plenary.nvim' },
+    { 'kyazdani42/nvim-web-devicons' },
+    { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    { 'nvim-telescope/telescope-live-grep-args.nvim' },
+  },
+  config = function()
+    require('user.plugins.telescope')
+  end,
+})
+
+use({
+  'morhetz/gruvbox',
+  config = function()
+    vim.cmd('colorscheme gruvbox')
+
+    -- Hide the characters in FloatBorder
+    vim.api.nvim_set_hl(0, 'FloatBorder', {
+      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+    })
+
+    -- Make the StatusLineNonText background the same as StatusLine
+    vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+      fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+      bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+    })
+
+    -- Hide the characters in CursorLineBg
+    vim.api.nvim_set_hl(0, 'CursorLineBg', {
+      fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+      bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+    })
+
+    vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#2F313C' })
+  end,
+})
+
+--
+
+require("bufferline").setup{}
+
+--
+
+-- Automatically install plugins on first run
+if packer_bootstrap then
+  require('packer').sync()
+end
+
+-- Automatically regenerate compiled loader file on save
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile>
+  augroup end
+]])
+
